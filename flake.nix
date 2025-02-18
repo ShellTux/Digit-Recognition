600c98ab1adb06c738b702fa595de885e0149f22
ShellTux <115948079+ShellTux@users.noreply.github.com>
2025-02-18 09:25:34 +0000

    add flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Matlab/GNU Octave Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages."${system}";
+    octave = pkgs.octaveFull.withPackages (octavePackages: with octavePackages; [
+      audio
+      image
+      linear-algebra
+      ltfat
+      matgeom
+      symbolic
+    ]);
+  in
+  {
+    devShells."${system}".default = pkgs.mkShell {
+      packages = [
+        octave
+        pkgs.entr
+      ];
+
+      INFOPATH = "${octave}/share/info";
+    };
+  };
+}
